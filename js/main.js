8882f07299c76e3b83714a04bc30fd20551c5720
msmpunk <msm_010_27_12@hotmail.com>
2018-08-04 18:39:31 -0500

    Aceptar solo numeros y cambios en el diseño

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 
-var table = [
+let table = [
   { nombre: 'MK', descargas: 10 },
   { nombre: 'PACMAN', descargas: 16 },
   { nombre: 'POKEMON', descargas: 18 },
   { nombre: 'DBZ', descargas: 17 }
-
 ]
+let saveStatuss = true;
 
-// window.onload = cargarEventos
+showTable();
 
-// function cargarEventos () {
-//   document.getElementById('show-table').addEventListener('click', showTable, false);
-
-//   document.getElementById('new-information').addEventListener('submit', newGame, false);
-
-//   document.getElementById('new-information').addEventListener('submit', deleteRow, false)
-// }
-showTable()
 function showTable() {
-  var tbody = document.getElementById('games-table');
-  var tableBody = ''
-
-  for (var i = 0; i < table.length; i++) {
-    tableBody += `<tr id="tr-${i}">
-                    <td id="td-n${i}">${table[i].nombre}</td>
-                    <td id="td-d${i}">${table[i].descargas}</td>
-                    <td><button class="btn-edit btn btn-secondary" value="${table[i].nombre}" onclick="editElment(this.value)">Editar</button></td>
-                    <td><button class="btn-delete btn btn-secondary" value="${table[i].nombre}" onclick="deleteRow(this.value)">Eliminar</button></td>
-                  <tr>`
-  }
+  let tbody = document.getElementById('games-table');
+  let tableBody = ''
+  if(table.length === 0){
+    tableBody = `<tr>
+                  <td></td>
+                  <td class="color-empty">No hay Elementos En la lista</td>
+                  <td></td>
+                <tr>`
+  }else{
+    for (let i = 0; i < table.length; i++) {
+      tableBody += `<tr id="tr-${i}">
+                      <td id="td-n${i}">${table[i].nombre}</td>
+                      <td id="td-d${i}">${table[i].descargas}</td>
+                      <td><button class="btn-edit btn btn-secondary" value="${table[i].nombre}" onclick="editElment(this.value)">Editar</button></td>
+                      <td><button class="btn-delete btn btn-secondary" value="${table[i].nombre}" onclick="deleteRow(this.value)">Eliminar</button></td>
+                    <tr>`
+    }
+  }    
   tbody.innerHTML = tableBody
 }
 
 function newGame() {
-  event.preventDefault()
-  var gameData = document.getElementById('game').value
-  var downloadsData = document.getElementById('downloads').value
+  event.preventDefault();
+  saveStatuss = true;
+  let gameData = document.getElementById('game').value
+  let downloadsData = document.getElementById('downloads').value
 
   const elementExist = existElment(gameData);
 
@@ -46,7 +46,7 @@ function newGame() {
   if (gameData === '' || downloadsData ==='') {
     alert('Por favor no dejes los campos vacios')
   } else {
-    var newGame = {nombre: gameData, descargas: downloadsData}
+    let newGame = {nombre: gameData, descargas: downloadsData}
     table.push(newGame)
     showTable();
   }
@@ -62,7 +62,7 @@ function existElment(value) {
 }
 
 function deleteRow(row) {
-
+  saveStatuss = true
   for (const i in table) {
     if (table[i].nombre === row) {
       table.splice(i, 1);
@@ -72,32 +72,36 @@ function deleteRow(row) {
 }
 
 function editElment(value) {
-  var place;
-  var nameGame = value;
-  var dowloads;
-  for (const i in table) {
-    if (table[i].nombre === nameGame) {
-      place = i;
-      dowloads = table[i].descargas;
+  if (saveStatuss === true) {
+    saveStatuss = false
+    let place;
+    let nameGame = value;
+    let dowloads;
+    for (const i in table) {
+      if (table[i].nombre === nameGame) {
+        place = i;
+        dowloads = table[i].descargas;
+      }
     }
-  }
-  let trNum = document.getElementById(`tr-${place}`);
+    let trNum = document.getElementById(`tr-${place}`);
+    
+    let newTr = `<tr id="tr-${place}">
+                  <td><input type="text" id="td-n${place}" value="${nameGame}"/></td>
+                  <td><input type="text" id="td-d${place}" onkeypress="return number(event)" value="${dowloads}"/></td>
+                  <td><button class="btn-save btn btn btn-primary" value="${place}" onclick="saveData(this.value)">Guardar</button></td>
+                  <td><button class="btn-edit btn btn-secondary" disabled value="${nameGame}" onclick="editElment(this.value)">Editar</button></td>
+                  <td><button class="btn-delete btn btn-secondary" disabled value="${nameGame}" onclick="deleteRow(this.value)">Eliminar</button></td>
+                <tr>`;
   
-  var newTr = `<tr id="tr-${place}">
-                <td><input type="text" id="td-n${place}" value="${nameGame}"/></td>
-                <td><input type="text" id="td-d${place}" value="${dowloads}"/></td>
-                <td><button class="btn-save btn btn btn-primary" value="${place}" onclick="saveData(this.value)">Guardar</button></td>
-                <td><button class="btn-edit btn btn-secondary" disabled value="${nameGame}" onclick="editElment(this.value)">Editar</button></td>
-                <td><button class="btn-delete btn btn-secondary" disabled value="${nameGame}" onclick="deleteRow(this.value)">Eliminar</button></td>
-              <tr>`;
+    trNum.innerHTML = newTr;
+  }
 
-  trNum.innerHTML = newTr;
 }
 
 function saveData(place) {
+  saveStatuss = true;
   let newName = document.getElementById(`td-n${place}`).value;
   let newNumDowloads = document.getElementById(`td-d${place}`).value;
-  console.log(newNumDowloads)
 
   const elementExist = existElment(newName);
 
@@ -117,4 +121,11 @@ function saveData(place) {
   }
 
   return alert('El nombre ya esta en la lista')
+}
+
+function number(e) {
+   let charCode = (e.which) ? e.which : event.keyCode
+   if (charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
+   return true;
 }
